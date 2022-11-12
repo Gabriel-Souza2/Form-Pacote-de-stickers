2c5e2e4f767ce84753dc9710b719a0aab4892d38
Gabriel-Souza2 <gabriel.souza2@hotmail.com.br>
2022-11-12 12:24:25 -0300

    💄 (theme): Adiconando tema padrão

---
+++ src/styles/default/theme.ts
@@ -0,0 +1,11 @@
+export const defaultTheme = {
+  white: '#FFFFFF',
+  'very-light-pink': '#F2F2F2',
+  'dark-blue-grey': '#191847',
+  'light-sky-blue': '#DDE3E9',
+  'greyish-brown': '#555555',
+  'rebecca-purple': '#663399',
+  'dark-grey-blue': '#2F3676',
+  'pine-green': '#071723',
+  'light-blue-grey': '#C5CFD6',
+}
