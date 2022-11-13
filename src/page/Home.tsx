c2f22421be3aec04bd3b133d668260049ae06fc8
Gabriel-Souza2 <gabriel.souza2@hotmail.com.br>
2022-11-13 19:25:24 -0300

    💄 (input checkbox): estilizando checkbox

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,4 +1,4 @@
-import { HomeContainer } from './styles'
+import { FormStickers, HomeContainer } from './styles'
 
 export function Home() {
   return (
@@ -15,23 +15,34 @@ export function Home() {
           </div>
         </header>
         <main>
-          <form>
+          <FormStickers>
             <fieldset>
               <legend>Quais Stickers?</legend>
               <div className="checkbox-wrapper">
                 <input type="checkbox" name="react" id="react" />
+                <div className="checkmark">
+                  <span className="check"></span>
+                </div>
+
                 <label htmlFor="react">React</label>
               </div>
               <div className="checkbox-wrapper">
                 <input type="checkbox" name="vue" id="vue" />
+                <div className="checkmark">
+                  <span className="check"></span>
+                </div>
                 <label htmlFor="vue">Vue</label>
               </div>
               <div className="checkbox-wrapper">
                 <input type="checkbox" name="angular" id="angular" />
+                <div className="checkmark">
+                  <span className="check"></span>
+                </div>
+
                 <label htmlFor="angular">Angular</label>
               </div>
             </fieldset>
-          </form>
+          </FormStickers>
         </main>
       </div>
     </HomeContainer>
