b8ce9140c0b190db7afc82266f125f51149ce1a4
Gabriel-Souza2 <gabriel.souza2@hotmail.com.br>
2022-11-15 23:18:19 -0300

    ✨ (AmountStickers): implementando o componente AmountStickers

--- src/components/AmountStickers/index.tsx
+++ src/components/AmountStickers/index.tsx
@@ -1,5 +1,16 @@
 import { AmountStickersContainer } from './styles'
 
 export function AmountStickers() {
-  return <AmountStickersContainer></AmountStickersContainer>
+  return (
+    <AmountStickersContainer>
+      <legend>Quantas Stickers de cada?</legend>
+      <button data-testid="btn-down">
+        <span className="sub"></span>
+      </button>
+      <input type="text" defaultValue={0} />
+      <button data-testid="btn-up">
+        <span className="add"></span>
+      </button>
+    </AmountStickersContainer>
+  )
 }
